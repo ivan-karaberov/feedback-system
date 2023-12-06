3e54f314f9031cbd534e7a94635409fb78ad0b50
ivan-karaberov <karaberovivan123456@gmail.com>
2023-12-06 20:49:34 +0300

    feat: add 404 page

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { notFoundComponent } from '../shared/components/notfound/notfound.component';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
@@ -8,7 +9,9 @@ const routes: Routes = [
   {path: '', component: AuthComponent, children: [
     {path: 'login', component: LoginComponent},
     {path: 'registration', component: RegistrationComponent}
-  ]}
+  ],},
+  
+  {path: '**', component: notFoundComponent}
 ];
 
 @NgModule({
